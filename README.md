935b7e173c4d1cbe68d6c99465694d16e95b1676
unknown <christopher.vaught@STLNHS-DW80298.na.corp.ipgnetwork.com>
2014-12-16 16:07:04 -0600

    Redesign of Cases Studies to use images as Background-Image proved to be difficult

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 NHS
 ===
+Note Jquery Mousewheel Plug-in
+https://github.com/jquery/jquery-mousewheel
+
+How to disable scrolling
+http://stackoverflow.com/questions/3656592/how-to-programmatically-disable-page-scrolling-with-jquery
--- js/app.js
+++ js/app.js
@@ -1,12 +1,54 @@
-$(document).ready(function(){
-//Go to Top of Page when Image Clicked
-    $(".backToTop").on('click', function(){
-     $("html, body").animate({ scrollTop: 0 }, "slow");
+function lockScroll() {
+    var scrollPosition = [
+        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+        self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      ];
+    var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+    html.data('scroll-position', scrollPosition);
+    html.data('previous-overflow', html.css('overflow'));
+    html.css('overflow', 'hidden');
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+    console.log("Lock Scroll");
+}
+
+function unlockScroll() {
+    var html = jQuery('html');
+    var scrollPosition = html.data('scroll-position');
+    html.css('overflow','auto');
+    window.scrollTo(scrollPosition[0], scrollPosition[1])
+    console.log("Unlock Scroll");
+}
+
+
+$(document).ready(function () {
+//Mousewheel event
+//    $('#Story').on('mousewheel', function (event) {
+//        console.log(event.deltaX, event.deltaY, event.deltaFactor);
+//        if (event.deltaY === -1) {
+//            //Lock scroll position      
+//            lockScroll();
+//        } else {
+//            unlockScroll();
+//        }
+//    });
+
+
+    //Go to Top of Page when Image Clicked
+    $(".backToTop").on('click', function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
     });
-//Scroll To Various Sections of Page
-    
-    
-//Need A Model Window to Attach to Current Parent Div Tag
-//Possibly Center
+    //Scroll To Various Sections of Page
+
+    //Listner Function for Case Studies   
+    //mouseover and mouseleave
+    $("#Work").on('mouseover', 'div', function () {
+//        var check = $(this).attr('class');
+        $(this).find(".overlayEffect").show();
+//        console.log("Hover action for " + check);
+    }).mouseout(function() {
+          $(this).find(".overlayEffect").hide();
+    });
 
 });
