47dfd99a90c3164921979df57f60ffcb788cefb9
unknown <christopher.vaught@STLNHS-DW80298.na.corp.ipgnetwork.com>
2014-12-15 18:04:26 -0600

    eod

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+//Go to Top of Page when Image Clicked
+    $(".backToTop").on('click', function(){
+     $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+//Scroll To Various Sections of Page
+    
+    
+//Need A Model Window to Attach to Current Parent Div Tag
+//Possibly Center
+
+});
